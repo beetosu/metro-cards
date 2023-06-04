4dc4f6a2883b2823e6e6fb16e6cb8e5245252809
beetosu <cpwalsh@buffalo.edu>
2023-06-04 13:50:38 -0400

    clean out default app stuff, add in some inital objects

---
+++ app/components/metrocard.tsx
@@ -0,0 +1,7 @@
+export default function MetroCard(stationCode: string) {
+    return (
+      <main>
+        <div className="hi">hello</div>
+      </main>
+    )
+  }
---
+++ app/enums/metro-line-enums.tsx
@@ -0,0 +1,10 @@
+enum MetroLines {
+    Silver = "SV",
+    Red = "RD",
+    Blue = "BL",
+    Orange = "OR",
+    Green = "GR",
+    Yellow = "YL"
+}
+
+export default MetroLines;
